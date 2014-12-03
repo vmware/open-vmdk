6a8cce21a278490a99e0de2b54156d70e6eb6685
Oliver Kurth <okurth@vmware.com>
2014-12-02 16:51:21 -0800

    add option to change num cpus and mem size (via environment variables)

--- ova/mkova.sh
+++ ova/mkova.sh
@@ -2,6 +2,9 @@
 
 TMPDIR=$(mktemp -p . -d XXXXXXXX)
 
+[ ! -n "$NUM_CPUS" ] && NUM_CPUS=2
+[ ! -n "$MEM_SIZE" ] && MEM_SIZE=1024
+
 if [ "$#" -ne 3 ] ; then
 	echo "$#"
 	echo usage "$0 name disk.vmdk template.ovf"
@@ -32,6 +35,8 @@ sed ${ovftempl} \
 	-e "s/@@NAME@@/${name}/g" \
 	-e "s/@@VMDK_FILE_SIZE@@/$VMDK_FILE_SIZE/g" \
 	-e "s/@@VMDK_CAPACITY@@/$VMDK_CAPACITY/g" \
+	-e "s/@@NUM_CPUS@@/$NUM_CPUS/g" \
+	-e "s/@@MEM_SIZE@@/$MEM_SIZE/g" \
 	> $TMPDIR/${name}.ovf
 
 echo "SHA1(${name}-disk1.vmdk)= $(sha1sum $TMPDIR/${name}-disk1.vmdk | cut -d' ' -f1)" > $TMPDIR/${name}.mf
@@ -41,4 +46,4 @@ pushd $TMPDIR
 tar cf ../${name}.ova *.ovf *.mf *.vmdk
 popd
 
-rm -rf $TMPDIR
+#rm -rf $TMPDIR
